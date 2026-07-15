95cf6d01dcfadf9e8b48b76cdfae14703442f2c3
Kiya Shahidi <kiyashahidi9@gmail.com>
2026-07-15 15:47:52 -0400

    update functionality with DB

--- index.js
+++ index.js
@@ -1,38 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const Note = require('./models/note')
+const Note = require('./models/note');
+const Person = require('../phonebook-backend-project/models/person');
 
 const app = express();
 
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
-
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET a POST are the most important methods of HTTP protocol",
-    important: true
-  }
-];
-
-function generateID() {
-  if (notes.length === 0) return 1;
-  let maxId = Math.max(...notes.map(n => Number(n.id)));
-
-  return String(maxId + 1)
-}
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -44,18 +20,25 @@ app.get('/api/notes', (req, res) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   const id = request.params.id
   Note.findById(id).then(note => {
-    response.json(note);
+    if (note) {
+      response.json(note);
+    } else {
+      response.status(404).end();
+    }
   })
+  .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/notes/:id', (request, response, next) => {
   const id = request.params.id;
-  notes = notes.filter(note => note.id !== id);
-
-  response.status(204).end();
+  Note.findByIdAndDelete(id)
+    .then(result => {
+      response.status(204).end();
+    })
+    .catch(error => next(error));
 })
 
 app.post('/api/notes', (request, response) => {
@@ -66,16 +49,45 @@ app.post('/api/notes', (request, response) => {
       error: "content missing :(",
     })
   }
-
+  
   let note = new Note({
     content: body.content,
     important: body.important || false,
   })
-
+  
   note.save().then(savedNote => {
     response.json(savedNote);
   })
 })
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const {content, important} = request.body;
+
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) return response.status(404).end();
+
+      note.content = content;
+      note.important = important;
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote);
+      })
+    })
+  .catch(error => next(error))
+})
+
+// ERROR MIDDLEWARE
+function errorHandler(error, request, response, next) {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted id'})
+  }
+
+  next(error);
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
